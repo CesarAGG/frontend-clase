f8bd5127a43da5e71ac6b76fcc7b1feb10fb44e6
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2024-04-24 14:22:35 -0600

    Video 5: Función Flecha, Arrow o Lambda

---
+++ 1-intro-js/src/02_funciones.js
@@ -0,0 +1,9 @@
+function obtenerInfo(name = "César", apellido = "Guerra") {
+  const info = `${name} ${apellido}`;
+
+  return info;
+}
+
+const info = obtenerInfo("Alex", "González");
+
+console.log(info)
---
+++ 1-intro-js/src/03_funcion_flecha.js
@@ -0,0 +1,7 @@
+const obtenerInfo = (name = "César", apellido = "Guerra") => `${name} ${apellido}`;
+const sum = (a = 0, b = 0) => a + b;
+
+const info = obtenerInfo("Alex", "González");
+
+console.log(info)
+console.log(20, 10);
