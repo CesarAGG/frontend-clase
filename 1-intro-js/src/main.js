064b8a192d59bc563c701e555cee71291c85be8b
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2024-04-24 12:58:55 -0600

    Video 2: Variables y Constantes

---
+++ 1-intro-js/src/main.js
@@ -0,0 +1,14 @@
+let firstName = "Gustavo"
+
+firstName = "César"
+
+const lastName = "Guerra"
+
+const condition = true;
+
+if (condition) {
+  const lastName = "González"
+  console.log(lastName)
+}
+
+console.log("Hola, mundo!" + firstName + ' ' + lastName)
