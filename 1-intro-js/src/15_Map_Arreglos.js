b4021f1f4d20949783dbf59e69fa2359bd882e77
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2024-04-24 23:48:51 -0600

    Video 18: Find, Filter y Some

--- 1-intro-js/src/15_Map_Arreglos.js
+++ 1-intro-js/src/15_Map_Arreglos.js
@@ -1,18 +1,16 @@
 const carros = [
     {
-        id: 1,
         marca: "Seat",
         modelo: "Fr",
         anio: "2018",
         nombre: "Leon",
         cliente: {
-            nombre: "Cesarr",
+            nombre: "Cesar",
             apellido: "Guerra"
         }
     }, {
-        id: 2,
         marca: "Seat",
-        modelo: "Excelence",
+        modelo: "Exce;;emce",
         anio: "2018",
         nombre: "Arona",
         cliente: {
@@ -20,7 +18,6 @@ const carros = [
             apellido: "Guerra"
         }
     }, {
-        id: 3,
         marca: "Cupra",
         modelo: "Cupra",
         anio: "2018",
@@ -30,7 +27,6 @@ const carros = [
             apellido: "Guerra"
         }
     }, {
-        id: 4,
         marca: "Cupra",
         modelo: "Cupra",
         anio: "2018",
@@ -40,7 +36,6 @@ const carros = [
             apellido: "Guerra"
         }
     }, {
-        id: 5,
         marca: "Seat",
         modelo: "Electrico",
         anio: "2018",
@@ -60,15 +55,3 @@ const nombresCarros = carros.map(e => e.cliente.nombre);
 
 console.log(marcasCarros);
 console.log(nombresCarros);
-
-const carroPorNombre = carros.find(e => e.cliente.nombre == "Cesar");
-
-console.log(carroPorNombre);
-
-const carroFilter = carros.filter(e => e.id != 1);
-
-console.log(carroFilter);
-
-const carroSome = carros.some(e => e.cliente.nombre == "Cessar");
-
-console.log(carroSome);
