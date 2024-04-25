8317c4f3e0094bc278d7a75514c6256101e02861
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2024-04-24 23:20:16 -0600

    Video 11: Operador Ternario

---
+++ 1-intro-js/src/08_Operador_Optional.js
@@ -0,0 +1,51 @@
+const carro = {
+  id: 1,
+  nombre: "LaFerrari",
+  date: new Date(),
+  marca: {
+    id: 1,
+    nombre: "Ferrari",
+    ubicacion: "Europa",
+    edad: 100
+  },
+  inventarios: [
+    {
+      id: 1,
+      color: "Azul",
+      cantidad: 5,
+      precio: 11000000
+    },
+    {
+      id: 2,
+      color: "Blanco",
+      cantidad: 2,
+      precio: 10000000
+    },
+    {
+      id: 3,
+      color: "Amarillo",
+      cantidad: 5,
+      precio: 11500000
+    }
+  ],
+  totalInventario: function () {
+    let total = 0;
+    this.inventarios.forEach(inventario => {
+      total = total + inventario.precio;
+    });
+    return total;
+  },
+  costo: 10000000,
+  saludo: function () {
+    return `Hola ${this.marca.nombre} ${this.nombre}`;
+  }
+}
+
+if (carro.lugarProcedencia != undefined && carro.lugarProcedencia.nombre) {
+  console.log("Cumple");
+} else {
+  console.log("No existe lugar de procedencia")
+}
+
+console.log(carro.lugarProcedencia);
+console.log(carro.lugarProcedencia?.municipio?.nombre);
--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,51 +1,18 @@
-const carro = {
-  id: 1,
-  nombre: "LaFerrari",
-  date: new Date(),
-  marca: {
-    id: 1,
-    nombre: "Ferrari",
-    ubicacion: "Europa",
-    edad: 100
-  },
-  inventarios: [
-    {
-      id: 1,
-      color: "Azul",
-      cantidad: 5,
-      precio: 11000000
-    },
-    {
-      id: 2,
-      color: "Blanco",
-      cantidad: 2,
-      precio: 10000000
-    },
-    {
-      id: 3,
-      color: "Amarillo",
-      cantidad: 5,
-      precio: 11500000
-    }
-  ],
-  totalInventario: function () {
-    let total = 0;
-    this.inventarios.forEach(inventario => {
-      total = total + inventario.precio;
-    });
-    return total;
-  },
-  costo: 10000000,
-  saludo: function () {
-    return `Hola ${this.marca.nombre} ${this.nombre}`;
-  }
-}
+const promedio = 7.0;
 
-if (carro.lugarProcedencia != undefined && carro.lugarProcedencia.nombre) {
-  console.log("Cumple");
-} else {
-  console.log("No existe lugar de procedencia")
-}
+let resultado = '';
 
-console.log(carro.lugarProcedencia);
-console.log(carro.lugarProcedencia?.municipio?.nombre);
+resultado = (promedio >= 7.0) ? "Aprobado" : "Reprobado";
+
+console.log(resultado)
+
+let valorMaximo = 0;
+
+const a = 4;
+const b = 6;
+const c = 3;
+
+valorMaximo = a > b ? a : b;
+valorMaximo = valorMaximo > c ? valorMaximo : c;
+
+console.log(valorMaximo);
