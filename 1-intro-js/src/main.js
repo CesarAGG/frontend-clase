02258451e4a5788e4bf7ec3aa3084328ddeadf3e
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2024-04-24 23:43:30 -0600

    Video 17: Función Map Arreglo

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,5 +1,57 @@
-const alumnos = ["monse", "mau", "gustavo", "jose", "barbara", "juan"];
+const carros = [
+    {
+        marca: "Seat",
+        modelo: "Fr",
+        anio: "2018",
+        nombre: "Leon",
+        cliente: {
+            nombre: "Cesar",
+            apellido: "Guerra"
+        }
+    }, {
+        marca: "Seat",
+        modelo: "Exce;;emce",
+        anio: "2018",
+        nombre: "Arona",
+        cliente: {
+            nombre: "Cesar",
+            apellido: "Guerra"
+        }
+    }, {
+        marca: "Cupra",
+        modelo: "Cupra",
+        anio: "2018",
+        nombre: "Ateca",
+        cliente: {
+            nombre: "Cesar",
+            apellido: "Guerra"
+        }
+    }, {
+        marca: "Cupra",
+        modelo: "Cupra",
+        anio: "2018",
+        nombre: "Formentor",
+        cliente: {
+            nombre: "Cesar",
+            apellido: "Guerra"
+        }
+    }, {
+        marca: "Seat",
+        modelo: "Electrico",
+        anio: "2018",
+        nombre: "Moto",
+        cliente: {
+            nombre: "Cesar",
+            apellido: "Guerra"
+        }
+    }
+]
 
-const [nom1, nom2, nom3, ...nombres] = alumnos;
+console.log(carros);
 
-console.log(nom1, nom2, nom3, nombres);
+const marcasCarros = carros.map(e => e.marca);
+
+const nombresCarros = carros.map(e => e.cliente.nombre);
+
+console.log(marcasCarros);
+console.log(nombresCarros);
