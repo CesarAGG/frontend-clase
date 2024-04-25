556e5bf31bc31a5fb57468073b25ba6e2cf59c17
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2024-04-24 23:57:23 -0600

    Video 20: Export Default

---
+++ 1-intro-js/src/17_Import_Export.js
@@ -0,0 +1,21 @@
+import carroPorNombre, { carros } from './data/carros'
+
+console.log(carros);
+
+const marcasCarros = carros.map(e => e.marca);
+
+const nombresCarros = carros.map(e => e.cliente.nombre);
+
+console.log(marcasCarros);
+console.log(nombresCarros);
+
+console.log("Carro por nombre: ");
+console.log(carroPorNombre("Cesar"));
+
+const carroFilter = carros.filter(e => e.id != 1);
+
+console.log(carroFilter);
+
+const carroSome = carros.some(e => e.cliente.nombre == "Cessar");
+
+console.log(carroSome);
